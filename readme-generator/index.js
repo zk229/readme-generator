42819e5db5adfeb00dab5984888cbf257748475f
zk229 <zkirsche01@gmail.com>
2022-02-27 17:35:50 -0500

    add questions

--- readme-generator/index.js
+++ readme-generator/index.js
@@ -1,13 +1,62 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Questions for user input
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe your project."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Describe how to install your project."
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Describe how your project should be used."
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "How can people contribute to your project?"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Provide instructions for testing the project."
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Choose a license.",
+        choices: ["MIT", "Apache", "GPL", "None"]
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your Github username."
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your e-mail address."
+    }
+];
 
-// TODO: Create a function to write README file
+// Write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
+// Initialize app
 function init() {}
 
-// Function call to initialize app
 init();
--- readme-generator/utils/generateMarkdown.js
+++ readme-generator/utils/generateMarkdown.js
@@ -1,16 +1,31 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license) {
+    return `https://img.shields.io/badge/License-${license}-blue`;
+  }
+  return "";
+}
 
-// TODO: Create a function that returns the license link
+// Returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license) {
 
-// TODO: Create a function that returns the license section of README
+  }
+  return "";
+}
+
+// Returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license) {
 
-// TODO: Create a function to generate markdown for README
+  }
+  return "";
+}
+
+// Generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
